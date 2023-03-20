b2a7fd70e45be678657857c336535994dad667d3
Elgzprtma <dikaelang13@gmail.com>
2023-03-20 14:43:38 +0700

    StudiKasus

---
+++ Level 1/FlowchartLevel1Task3.php
@@ -0,0 +1,20 @@
+<?php
+$bilangan1 = 31;
+$bilangan2 = 102;
+$bilangan3 = 1221;
+$bilangan4 = 1231;
+
+$terbesar = $bilangan1;
+if ($bilangan2 > $terbesar) {
+    $terbesar = $bilangan2;
+}
+if ($bilangan3 > $terbesar) {
+    $terbesar = $bilangan3;
+}
+if ($bilangan4 > $terbesar) {
+    $terbesar = $bilangan4;
+}
+
+
+echo "Bilangan terbesar adalah: " . $terbesar;
+?>
